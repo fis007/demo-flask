5d35a0144558874d78b7f73446073a7c2fcb35bb
fis007 <schaffhauserfilip@gmail.com>
2020-10-14 14:53:07 +0800

    passed variable to template

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-
-@app.route('/user/<username>')
-def show(username):
-    return f"Hi {username[5]}"
-
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run()
